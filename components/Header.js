f5ee1b70f1bc6b3897466dc976aad38435f7710d
infinite404 <balazs.hende@gmail.com>
2021-02-28 16:59:19 +0100

    GA tag updated

--- components/Header.js
+++ components/Header.js
@@ -52,13 +52,13 @@ class HeadElement extends React.Component {
         <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossOrigin="true"></script>
         <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="true"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3ET647PCS7"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8D4K06TM96"></script>
         <script dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'GA_MEASUREMENT_ID');`
+          gtag('config', 'G-8D4K06TM96');`
 	}}
         />
         </Head>
