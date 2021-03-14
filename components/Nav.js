83a4e6c26f36330c84e9b4eef7a5305ca2f28a2f
Szabolcs Szilágyi <szabolcs.szilagyi@planorama.com>
2021-03-14 09:01:46 +0100

    remove banner and cleanup unused method

--- components/Nav.js
+++ components/Nav.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import {API_SERVER as API_SERVER} from '../src/constants';
+
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 const session = cookies.get('session');
 
-class Banner extends Component {
-  render() {
-    if (Date.now() > new Date('2021-01-01')) return (<div></div>);
-
-    return (
-      <div id="banner" className="row">
-        Enjoy FREE delivery in December
-      </div>
-    );
-  }
-}
+import {
+  API_SERVER
+} from '../src/constants';
 
 export default class Nav extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       myEmail: '',
       cart: '/cart.png',
       inCart: '0'
     };
+
     this.amILoggedIn = this. amILoggedIn.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.cartHover = this.cartHover.bind(this);
     this.cartNormal = this.cartNormal.bind(this);
     this.getInCart = this.getInCart.bind(this);
   }
+
   amILoggedIn () {
     fetch(API_SERVER + 'listen.php?part=amiloggedin&sessiontoken=' + session)
     .then(response => response.json())
@@ -43,14 +37,15 @@ export default class Nav extends Component {
     })
     .catch(error => console.log(error.message));
   }
-  handleChange () {
-  }
+
   cartHover () {
     this.setState({ cart: '/cart-b.png' });
   }
+
   cartNormal () {
     this.setState({ cart: '/cart.png' });
   }
+
   getInCart () {
     fetch(API_SERVER + 'listen.php?part=getproductsnumberincart&sessiontoken=' + session)
     .then(response => response.json())
@@ -61,13 +56,16 @@ export default class Nav extends Component {
     })
     .catch(error => console.log(error.message));
   }
+
   componentDidUpdate() {
     this.getInCart ();
   }
+
   componentDidMount() {
     var element = document.getElementById("dropdown-custom-components");
     this.getInCart ();
   }
+
   render() {
     return (
       <div>
@@ -90,12 +88,20 @@ export default class Nav extends Component {
                   <a className="menu" href="/my-account">My Account</a>
                 </div>
                 <div className="col-md-5 ce padtop43px navCart">
-                  <a className="menu" href="/checkout" onMouseEnter={this.cartHover} onMouseLeave={this.cartNormal}><img src={this.state.cart} width="35" height="35" />({this.state.inCart})</a>
+                  <a
+                    className="menu"
+                    href="/checkout"
+                    onMouseEnter={this.cartHover}
+                    onMouseLeave={this.cartNormal}
+                  ><img
+                     src={this.state.cart}
+                     width="35"
+                     height="35"
+                   />({this.state.inCart})</a>
                 </div>
               </div>
             </div>
           </div>
-          <Banner />
         </div>
         <div className="tabNav">
           <div className="row">
@@ -108,7 +114,18 @@ export default class Nav extends Component {
               <div className="padtop50px blackFont"><a href="/my-account">My Account</a></div>
             </div>
             <div className="col-md-4 ce">
-              <div className="padtop43px blackFont"><a className="padtop43px" href="/checkout" onMouseEnter={this.cartHover} onMouseLeave={this.cartNormal}><img src={this.state.cart} width="35" height="35" />({this.state.inCart})</a></div>
+              <div
+                className="padtop43px blackFont"
+              ><a
+                 className="padtop43px"
+                 href="/checkout"
+                 onMouseEnter={this.cartHover}
+                 onMouseLeave={this.cartNormal}
+               ><img
+                  src={this.state.cart}
+                  width="35"
+                  height="35"
+                />({this.state.inCart})</a></div>
             </div>
           </div>
           <div className="row">
@@ -125,14 +142,23 @@ export default class Nav extends Component {
               <div className="padtop43px blackFont"><a href="/our-story">Our Story</a></div>
             </div>
           </div>
-          <Banner />
         </div>
         <div className="mobileNav">
           <div className="row">
             <div className="col-md-12 ce">
               <a href="/"><img src="/logo.png" alt="MYNA logo" /></a>
               <div className="blackFont"><a href="/my-account">My Account</a></div>
-              <div className="blackFont padtop10px"><a href="/checkout" onMouseEnter={this.cartHover} onMouseLeave={this.cartNormal}><img src={this.state.cart} width="35" height="35" />({this.state.inCart})</a></div>
+              <div
+                className="blackFont padtop10px"
+              ><a
+                 href="/checkout"
+                 onMouseEnter={this.cartHover}
+                 onMouseLeave={this.cartNormal}
+               ><img
+                  src={this.state.cart}
+                  width="35"
+                  height="35"
+                />({this.state.inCart})</a></div>
             </div>
           </div>
           <div className="row">
@@ -143,7 +169,6 @@ export default class Nav extends Component {
               <div className="blackFont padtop10px"><a href="/our-story">Our Story</a></div>
             </div>
           </div>
-          <Banner />
         </div>
       </div>
     );
