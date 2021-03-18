3538b7f891bf7ac7d694e7ae32131fce2acb1238
Szabolcs Szilágyi <szabolcs.szilagyi@planorama.com>
2021-03-18 09:12:58 +0100

    change Slider component from class to function

--- components/Slider.js
+++ components/Slider.js
@@ -42,27 +42,25 @@ const properties = {
   pauseOnHover: true,
 };
 
-export default class Slider extends Component {
-    render () {
-      return (
-        <div className={styles.slideContainer}>
-          <Slide {...properties}>
-            {slides.map(({ text, linkText, linkHref, imageSrc, buttonClass }, i) => (
-              <div key={`slider-${i}`} className={styles.eachSlide}>
-                  <div className={styles.textcontainer}>
-                    <Image
-                      src={imageSrc}
-                      layout="fill"
-                      objectFit="cover"
-                      objectPosition="left top"
-                    />
-                    <h3>{text}</h3>
-                    <a href={linkHref} className={buttonClass}>{linkText}</a>
-                  </div>
-              </div>
-            ))}
-          </Slide>
-        </div>
-      )
-    }
+export default function Slider() {
+  return (
+    <div className={styles.slideContainer}>
+      <Slide {...properties}>
+        {slides.map(({ text, linkText, linkHref, imageSrc, buttonClass }, i) => (
+          <div key={`slider-${i}`} className={styles.eachSlide}>
+            <div className={styles.textcontainer}>
+              <Image
+                src={imageSrc}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="left top"
+              />
+              <h3>{text}</h3>
+              <a href={linkHref} className={buttonClass}>{linkText}</a>
+            </div>
+          </div>
+        ))}
+      </Slide>
+    </div>
+  )
 }
