3f76290589a17205a3cf152eea88eedcc55d4bac
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-04-25 20:52:24 +0200

    Finalizing for staging

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const configuration = {
     PAY_PAL_CLIENT_ID: 'AWhXmi7ecVKx2DNiRBL15o-EoxX4hS52H09EB3SrdPAeSE01AvlE2dMqZa8rsDy7yeFHThNc9vULo06W',
   },
   staging: {
-    SERVER_ADDRESS: 'https://mynalabel-staging.herokuapp.com/',
-    API_ADDRESS: 'https://mynalabel-staging.herokuapp.com/',
+    SERVER_ADDRESS: 'https://api-staging.mynalabel.com/',
+    API_ADDRESS: 'https://api-staging.mynalabel.com/',
     API_PATH: 'legacy',
     EMAIL_PATH: 'legacy',
     PAY_PAL_CLIENT_ID: 'AWhXmi7ecVKx2DNiRBL15o-EoxX4hS52H09EB3SrdPAeSE01AvlE2dMqZa8rsDy7yeFHThNc9vULo06W',
