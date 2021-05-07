a37691f3d0160bb9a7ed0dc56fe4b1656e1b30c1
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-05-07 19:48:03 +0200

    Product listing to use simple lists
    Instead of "manually" listing the products in tsx, use an array for each
    collection and just partition it per twos.

--- src/pages/shop-collections.tsx
+++ src/pages/shop-collections.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import Ping from '../components/Ping';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import SingleProductCard from '../components/SingleProductCard';
+import { chunk } from 'lodash';
 
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
-const hash = cookies.get('hash');
+type Product = {
+  link: string,
+  mainPhoto: string,
+  name: string,
+  price: string,
+};
+
+function createProductCard(product: Product) {
+  if(!product) return (<></>);
+  return (
+    <SingleProductCard
+      productPageLink={product.link}
+      productImageLink={product.mainPhoto}
+      productName={product.name}
+      price={product.price}
+    />
+  );
+}
+
+function groupProducts(products: Product[]) {
+  const grouped = chunk(products, 2).map(([firstProduct, secondProduct], i) => {
+    return (
+      <div key={`${firstProduct.name}-${i}`} className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <div className="row">
+            {createProductCard(firstProduct)}
+            {createProductCard(secondProduct)}
+          </div>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+    );
+  });
+
+  return (<>{grouped}</>);
+}
 
 export default class Index extends React.Component {
   render() {
@@ -47,88 +80,62 @@ export default class Index extends React.Component {
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/reeva-denim-jacket"
-                productImageLink="/product_photos/reeva-denim-jacket-01.jpg"
-                productName="REEVA Denim Jacket"
-                price="€159"
-              />
-              <SingleProductCard
-                productPageLink="/senna-skirt"
-                productImageLink="/product_photos/senna-skirt-01.jpg"
-                productName="SENNA Skirt"
-                price="€135"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/tuli-dress"
-                productImageLink="/product_photos/tuli-dress-01.jpg"
-                productName="TULI Dress"
-                price="€169"
-              />
-              <SingleProductCard
-                productPageLink="/leya-wrap-dress"
-                productImageLink="/product_photos/leya-wrap-dress-01.jpg"
-                productName="LEYA Wrap Dress"
-                price="€319"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/dahlia-blouse"
-                productImageLink="/product_photos/dahlia-blouse-01.jpg"
-                productName="DAHLIA Blouse"
-                price="€105"
-              />
-              <SingleProductCard
-                productPageLink="/bella-hand-painted-blouse"
-                productImageLink="/product_photos/bella-print-01.jpg"
-                productName="BELLA Hand Painted Blouse"
-                price="€129"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/bella-blouse"
-                productImageLink="/product_photos/bella-blouse-01.jpg"
-                productName="BELLA Blouse"
-                price="€79"
-              />
-              <SingleProductCard
-                productPageLink="/delphi-culottes"
-                productImageLink="/product_photos/delphi-culottes-01.jpg"
-                productName="DELPHI Culottes"
-                price="€95"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
+        {(groupProducts([
+          {
+            name: 'LISIA Dress',
+            link: 'lisia-dress',
+            mainPhoto: '/product_photos/lisia-dress-01.jpg',
+            price: '€179'
+          },
+          {
+            name: "SENNA Skirt",
+            link: "/senna-skirt",
+            mainPhoto: "/product_photos/senna-skirt-01.jpg",
+            price: "€135",
+          },
+          {
+            name: "REEVA Denim Jacket",
+            link: "/reeva-denim-jacket",
+            mainPhoto: "/product_photos/reeva-denim-jacket-01.jpg",
+            price: "€159",
+          },
+          {
+            name: "TULI Dress",
+            link: "/tuli-dress",
+            mainPhoto: "/product_photos/tuli-dress-01.jpg",
+            price: "€169"
+          },
+          {
+            name: "LEYA Wrap Dress",
+            link: "/leya-wrap-dress",
+            mainPhoto: "/product_photos/leya-wrap-dress-01.jpg",
+            price: "€319",
+          },
+          {
+            name: "DAHLIA Blouse",
+            link: "/dahlia-blouse",
+            mainPhoto: "/product_photos/dahlia-blouse-01.jpg",
+            price: "€105",
+          },
+          {
+            name: "BELLA Hand Painted Blouse",
+            link: "/bella-hand-painted-blouse",
+            mainPhoto: "/product_photos/bella-print-01.jpg",
+            price: "€129",
+          },
+          {
+            name: "BELLA Blouse",
+            link: "/bella-blouse",
+            mainPhoto: "/product_photos/bella-blouse-01.jpg",
+            price: "€79",
+          },
+          {
+            name: "DELPHI Culottes",
+            link: "/delphi-culottes",
+            mainPhoto: "/product_photos/delphi-culottes-01.jpg",
+            price: "€95",
+          },
+        ]))}
 
         <div className="spacer50px" />
         <div className="row">
@@ -136,66 +143,45 @@ export default class Index extends React.Component {
             <h5><strong>Love and Light</strong></h5>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/lotus-sand"
-                productImageLink="/product_photos/mynawebshop-whitedress-1.jpg"
-                productName="Lotus Woven Dress"
-                price="€225"
-              />
-              <SingleProductCard
-                productPageLink="/ivy-cream"
-                productImageLink="/product_photos/mynawebshop-whitetshirt-1.jpg"
-                productName="Ivy Knitted Jersey Top"
-                price="€75"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/aster-green"
-                productImageLink="/product_photos/mynawebshop-greenpants-1.jpg"
-                productName="Aster Green"
-                price="€139"
-              />
-              <SingleProductCard
-                productPageLink="/aster-sand"
-                productImageLink="/product_photos/mynawebshop-linenpants-1.jpg"
-                productName="Aster Sand"
-                price="€139"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/gea-cream"
-                productImageLink="/product_photos/mynawebshop-whitetop-1.jpg"
-                productName="Gea Knitted Jersey Top"
-                price="€75"
-              />
-              <SingleProductCard
-                productPageLink="/nolia-dustpink"
-                productImageLink="/product_photos/mynawebshop-pinkdress-1.jpg"
-                productName="Nolia Woven Dress"
-                price="€215"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
+
+        {(groupProducts([
+          {
+            name: "Lotus Woven Dress",
+            link: "/lotus-sand",
+            mainPhoto: "/product_photos/mynawebshop-whitedress-1.jpg",
+            price: "€225",
+          },
+          {
+            link: "/ivy-cream",
+            mainPhoto: "/product_photos/mynawebshop-whitetshirt-1.jpg",
+            name: "Ivy Knitted Jersey Top",
+            price: "€75",
+          },
+          {
+            link: "/aster-green",
+            mainPhoto: "/product_photos/mynawebshop-greenpants-1.jpg",
+            name: "Aster Green",
+            price: "€139",
+          },
+          {
+            link: "/aster-sand",
+            mainPhoto: "/product_photos/mynawebshop-linenpants-1.jpg",
+            name: "Aster Sand",
+            price: "€139",
+          },
+          {
+            link: "/gea-cream",
+            mainPhoto: "/product_photos/mynawebshop-whitetop-1.jpg",
+            name: "Gea Knitted Jersey Top",
+            price: "€75",
+          },
+          {
+            link: "/nolia-dustpink",
+            mainPhoto: "/product_photos/mynawebshop-pinkdress-1.jpg",
+            name: "Nolia Woven Dress",
+            price: "€215",
+          },
+        ]))}
 
         <div className="spacer50px" />
         <div className="row">
@@ -203,46 +189,33 @@ export default class Index extends React.Component {
             <h5><strong>Love Affair Collection</strong></h5>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/alyss-dress"
-                productImageLink="/product_photos/mynawebshop-alyssdress-1.jpg"
-                productName="Alyss Dress"
-                price="€215"
-              />
-              <SingleProductCard
-                productPageLink="/calla-cream"
-                productImageLink="/product_photos/mynawebshop-whitejeans-1.jpg"
-                productName="Calla Denim Pants"
-                price="€155"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="row">
-              <SingleProductCard
-                productPageLink="/tilja-top"
-                productImageLink="/product_photos/mynawebshop-tiljatop-1.jpg"
-                productName="Tilia Top"
-                price="€115"
-              />
-              <SingleProductCard
-                productPageLink="/magna-scarf"
-                productImageLink="/product_photos/mynawebshop-magnascarf-1.jpg"
-                productName="Magna Scarf"
-                price="€99"
-              />
-            </div>
-          </div>
-          <div className="col-md-2"></div>
-        </div>
+
+        {(groupProducts([
+          {
+            link: "/alyss-dress",
+            mainPhoto: "/product_photos/mynawebshop-alyssdress-1.jpg",
+            name: "Alyss Dress",
+            price: "€215",
+          },
+          {
+            link: "/calla-cream",
+            mainPhoto: "/product_photos/mynawebshop-whitejeans-1.jpg",
+            name: "Calla Denim Pants",
+            price: "€155",
+          },
+          {
+            link: "/tilja-top",
+            mainPhoto: "/product_photos/mynawebshop-tiljatop-1.jpg",
+            name: "Tilia Top",
+            price: "€115",
+          },
+          {
+            link: "/magna-scarf",
+            mainPhoto: "/product_photos/mynawebshop-magnascarf-1.jpg",
+            name: "Magna Scarf",
+            price: "€99",
+          },
+        ]))}
 
         <Footer />
       </Container>
