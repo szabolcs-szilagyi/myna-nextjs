58b3e516152c9ae1b24a12176e94bb50dc5921c6
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-08-20 20:10:24 +0200

    chore: indented product page

--- src/pages/[idname].tsx
+++ src/pages/[idname].tsx
@@ -89,35 +89,35 @@ export async function getStaticPaths() {
 
 export default function Index (props: any) {
   const router = useRouter();
-    const idName = router.query.idname;
-    const isOneSize = props.isOneSize;
+  const idName = router.query.idname;
+  const isOneSize = props.isOneSize;
 
-    const [state, setState] = useState({
-      cartButtonVisibility: 'visible',
-      addToCart: 'ADD TO CART',
-      productIdToCart: '',
-      idName,
-      currency: '€',
-      avby: DEFAULT_AVAILABLE,
-      productName: '',
-      productColor: '',
-      productPrice: '',
-      description: '',
-      compCare: '',
-      photos: {
-        photo1: '',
-        photo2: '',
-        photo3: '',
-        photo4: '',
-        photo5: '',
-        photo6: '',
-        photo7: '',
-        photo8: '',
-        photo9: '',
-      },
-      lastItemsDate: null,
-      ...props
-    });
+  const [state, setState] = useState({
+    cartButtonVisibility: 'visible',
+    addToCart: 'ADD TO CART',
+    productIdToCart: '',
+    idName,
+    currency: '€',
+    avby: DEFAULT_AVAILABLE,
+    productName: '',
+    productColor: '',
+    productPrice: '',
+    description: '',
+    compCare: '',
+    photos: {
+      photo1: '',
+      photo2: '',
+      photo3: '',
+      photo4: '',
+      photo5: '',
+      photo6: '',
+      photo7: '',
+      photo8: '',
+      photo9: '',
+    },
+    lastItemsDate: null,
+    ...props
+  });
   const [selectedSize, setSelectedSize] = useState(isOneSize ? 'one_size' : '0');
 
   function defaultButton() {
@@ -132,30 +132,30 @@ export default function Index (props: any) {
     if (size === '0') return;
 
     fetch(API_SERVER + API_PATH + '?part=availabilityexact&idname=' + idName + '&size=' + size + '&sessiontoken=' + session)
-    .then(response => response.json())
-		.then(output => {
-      const availability = output && output.availability;
-      if (availability > 0) {
-        setState({
-          ...state,
-          avby: DEFAULT_AVAILABLE,
-          cartButtonVisibility: 'visible',
-        });
-      } else if(availability === 0) {
-        setState({
-          ...state,
-          avby: 'Pre-Order / Contact Us',
-          cartButtonVisibility: 'invisible',
-        });
-      } else if(availability === null) {
-        setState({
-          ...state,
-          avby: 'Not Available',
-          cartButtonVisibility: 'invisible',
-        });
-      }
-    })
-    .catch(error => console.log(error.message));
+      .then(response => response.json())
+      .then(output => {
+        const availability = output && output.availability;
+        if (availability > 0) {
+          setState({
+            ...state,
+            avby: DEFAULT_AVAILABLE,
+            cartButtonVisibility: 'visible',
+          });
+        } else if(availability === 0) {
+          setState({
+            ...state,
+            avby: 'Pre-Order / Contact Us',
+            cartButtonVisibility: 'invisible',
+          });
+        } else if(availability === null) {
+          setState({
+            ...state,
+            avby: 'Not Available',
+            cartButtonVisibility: 'invisible',
+          });
+        }
+      })
+      .catch(error => console.log(error.message));
   }
 
   function addToCart() {
@@ -164,12 +164,12 @@ export default function Index (props: any) {
       if (size != '0') {
         let idName = state.idName;
         fetch(API_SERVER + API_PATH + '?part=addproducttocart&idname=' + idName + '&size=' + size + '&sessiontoken=' + session)
-        .then(response => response.json())
-  		  .then(output => {
-          let data = output;
-          let tmp = data['success'];
-        })
-        .catch(error => console.log(error.message));
+          .then(response => response.json())
+          .then(output => {
+            let data = output;
+            let tmp = data['success'];
+          })
+          .catch(error => console.log(error.message));
         setState({
           ...state,
           addToCart: 'ADDED TO CART',
@@ -182,31 +182,31 @@ export default function Index (props: any) {
 
   function loadData() {
     fetch(API_SERVER + API_PATH + '?part=getproductdata&productname=' + state.idName)
-    .then(response => response.json())
-		.then(({ productdetails }) => {
-      setState({
-        ...state,
-        productIdToCart: productdetails.id,
-        productName: productdetails.productname,
-        productColor: productdetails.productcolor,
-        productPrice: productdetails.productprice,
-        description: productdetails.desclong,
-        compCare: productdetails.compcare,
-        availability: productdetails.availability,
-        photos: {
-          photo1: productdetails.pic1,
-          photo2: productdetails.pic2,
-          photo3: productdetails.pic3,
-          photo4: productdetails.pic4,
-          photo5: productdetails.pic5,
-          photo6: productdetails.pic6,
-          photo7: productdetails.pic7,
-          photo8: productdetails.pic8,
-          photo9: productdetails.pic9,
-        },
-      });
-    })
-    .catch(error => console.log(error.message));
+      .then(response => response.json())
+      .then(({ productdetails }) => {
+        setState({
+          ...state,
+          productIdToCart: productdetails.id,
+          productName: productdetails.productname,
+          productColor: productdetails.productcolor,
+          productPrice: productdetails.productprice,
+          description: productdetails.desclong,
+          compCare: productdetails.compcare,
+          availability: productdetails.availability,
+          photos: {
+            photo1: productdetails.pic1,
+            photo2: productdetails.pic2,
+            photo3: productdetails.pic3,
+            photo4: productdetails.pic4,
+            photo5: productdetails.pic5,
+            photo6: productdetails.pic6,
+            photo7: productdetails.pic7,
+            photo8: productdetails.pic8,
+            photo9: productdetails.pic9,
+          },
+        });
+      })
+      .catch(error => console.log(error.message));
   }
 
   function handleSizeChange(e) {
@@ -215,90 +215,90 @@ export default function Index (props: any) {
     checkAvailability(newSize, session, state.idName);
   }
 
-		return (
-      <Container fluid>
-        <Header />
-        <Nav lastItemsDate={state.lastItemsDate} />
-        <Ping />
-        <div className="spacer50px"></div>
-        <div className="row">
-          <div className="col-md-1"></div>
-          <div className="col-md-10">
-            <div className="row">
+  return (
+    <Container fluid>
+      <Header />
+      <Nav lastItemsDate={state.lastItemsDate} />
+      <Ping />
+      <div className="spacer50px"></div>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-10">
+          <div className="row">
 
-              <PhotoViewer
-                photos={state.photos}
+            <PhotoViewer
+              photos={state.photos}
+            />
+
+            <div className="col-md-6 ce">
+              <div className="row">
+                <div className="col-md-12">
+                  <h1 className="capitalLetters">{state.productName} | {state.productColor} | {state.currency}{state.productPrice}</h1>
+                </div>
+              </div>
+              <div className="spacer50px"></div>
+
+              <ProductInfo
+                description={state.description}
+                compCare={state.compCare}
               />
 
-              <div className="col-md-6 ce">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h1 className="capitalLetters">{state.productName} | {state.productColor} | {state.currency}{state.productPrice}</h1>
-                  </div>
-                </div>
-                <div className="spacer50px"></div>
-
-                <ProductInfo
-                  description={state.description}
-                  compCare={state.compCare}
-                />
-
-                <div className="spacer50px"></div>
-                <div className="row">
-                  <div className="col-md-1"></div>
-                  <div className="col-md-6 left">
-                    <select
-                      id="chooseSize"
-                      className={state.isOneSize ?
-                                 'sizeButton invisible' :
-                                 'sizeButton'}
-                      value={selectedSize}
-                      onChange={handleSizeChange}
-                    >
-                      <option value="0">CHOOSE SIZE</option>
-                      <option value="xs">XS</option>
-                      <option value="s">S</option>
-                      <option value="m">M</option>
-                      <option value="ml">ML</option>
-                      <option value="l">L</option>
-										</select>
-                    <div className="spacer25px"></div>
-                    <div className={state.cartButtonVisibility}>
-                      <div className="noBorder mediumFont">
-                        <button
-                          type="button"
-                          className="cartButton"
-                          onClick={addToCart}
-                        >{state.addToCart}</button>
-                      </div>
+              <div className="spacer50px"></div>
+              <div className="row">
+                <div className="col-md-1"></div>
+                <div className="col-md-6 left">
+                  <select
+                    id="chooseSize"
+                    className={state.isOneSize ?
+                               'sizeButton invisible' :
+                               'sizeButton'}
+                    value={selectedSize}
+                    onChange={handleSizeChange}
+                  >
+                    <option value="0">CHOOSE SIZE</option>
+                    <option value="xs">XS</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="ml">ML</option>
+                    <option value="l">L</option>
+                  </select>
+                  <div className="spacer25px"></div>
+                  <div className={state.cartButtonVisibility}>
+                    <div className="noBorder mediumFont">
+                      <button
+                        type="button"
+                        className="cartButton"
+                        onClick={addToCart}
+                      >{state.addToCart}</button>
                     </div>
                   </div>
-                  <div className="col-md-4">
-                    <div className="capitalLetters pad8px">
-                      {state.avby}
-                    </div>
-                  </div>
-                  <div className="col-md-1"></div>
                 </div>
-                <div className="spacer25px"></div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="productInfoContainer noBorder mediumFont">
-                      <div className="mediumFont ju">
-                        Each item is created by our talented creative director Justyna and handmade
-                        with care in Poland. If you cannot find your size, get in touch with us and
-                        we will do our best to help. Email us on <a href="mailto:connect@mynalabel.com" className="blackFont">connect@mynalabel.com</a> or click <a className="blackFont" href="/shipping">here</a> for more information about orders.
-                      </div>
+                <div className="col-md-4">
+                  <div className="capitalLetters pad8px">
+                    {state.avby}
+                  </div>
+                </div>
+                <div className="col-md-1"></div>
+              </div>
+              <div className="spacer25px"></div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="productInfoContainer noBorder mediumFont">
+                    <div className="mediumFont ju">
+                      Each item is created by our talented creative director Justyna and handmade
+                      with care in Poland. If you cannot find your size, get in touch with us and
+                      we will do our best to help. Email us on <a href="mailto:connect@mynalabel.com" className="blackFont">connect@mynalabel.com</a> or click <a className="blackFont" href="/shipping">here</a> for more information about orders.
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-1"></div>
         </div>
-        <div className="spacer50px"></div>
-        <Footer />
-      </Container>
-    );
+        <div className="col-md-1"></div>
+      </div>
+      <div className="spacer50px"></div>
+      <Footer />
+    </Container>
+  );
 }
