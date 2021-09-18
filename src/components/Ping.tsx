3e346aa7fd7c69d2ee7ec1fd2360f77d6b1b9d9a
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-09-18 08:26:47 +0200

    refactor: changed Ping to function component

--- src/components/Ping.tsx
+++ src/components/Ping.tsx
@@ -1,54 +1,46 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import Cookies from 'universal-cookie';
 import {
   API_SERVER,
   API_PATH,
 } from '../constants';
 const cookies = new Cookies();
-//const hash = cookies.get('hash');
 const session = cookies.get('session');
 
-export default class Ping extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      session: ''
-    };
-    this.ping = this.ping.bind(this);
-    this.redirect = this.redirect.bind(this);
-  }
-  ping() {
-    if (session == 'undefined') {
+export default function Ping() {
+
+  function doPing() {
+    if(session == 'undefined') {
       fetch(API_SERVER + API_PATH + '?part=ping')
-      .then(response => response.json())
-  		.then(output => {
-        let data = output;
-        let tmp = data['sessiontoken'];
-        cookies.set("session", tmp, { maxAge: 1800 });
-      })
-      .catch(error => console.log(error.message));
-      setTimeout(this.redirect, 1000);
+        .then(response => response.json())
+        .then(({ sessiontoken }) => {
+          cookies.set('session', sessiontoken, { maxAge: 1800 });
+        })
+        .catch(error => console.log(error.message));
+      setTimeout(redirect, 1000);
     }
-    if (session != 'undefined') {
+
+    if(session != 'undefined') {
       fetch(API_SERVER + API_PATH + '?part=ping&sessiontoken=' + session)
-      .then(response => response.json())
-  		.then(output => {
-        let data = output;
-        let tmp = data['sessiontoken'];
-        cookies.set("session", tmp, { maxAge: 1800 });
-      })
-      .catch(error => console.log(error.message));
+        .then(response => response.json())
+        .then(({ sessiontoken }) => {
+          cookies.set("session", sessiontoken, { maxAge: 1800 });
+        })
+        .catch(error => console.log(error.message));
     }
   }
-  redirect () {
+
+  function redirect () {
     window.location.href = "/";
   }
-  componentDidMount() {
-    this.ping ();
+
+  function componentDidMount() {
+    doPing();
   }
-  render() {
-    return (
-      <div></div>
-    );
-  }
+
+  useEffect(componentDidMount, []);
+
+  return (
+    <div></div>
+  );
 }
