3b6885484fb90e712ffdd11d12ff19cacc3b0338
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-10-08 23:52:09 +0200

    chore: remove the promo-slide aswell

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -19,50 +19,10 @@ const properties = {
   pauseOnHover: true,
 };
 
-function ImageElement({
-  imageSrc,
-  imageHref,
-}) {
-  if(imageHref) {
-    return (
-      <Link href={imageHref}>
-        <a>
-          <Image
-            quality={90}
-            src={imageSrc}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="left top"
-          />
-        </a>
-      </Link>
-    );
-  } else {
-    return (
-      <Image
-        quality={90}
-        src={imageSrc}
-        layout="fill"
-        objectFit="cover"
-        objectPosition="left top"
-      />
-    );
-  }
-}
-
 export default function Slider() {
   const { t } = useTranslation('slider');
 
   const slides = [
-    {
-      text: '',
-      linkText: '',
-      linkHref: '/shop-collections',
-      imageHref: '/shop-collections',
-      imageSrc: '/slides/04.jpg',
-      paragraphClass: 'd-none',
-      buttonClass: 'd-none',
-    },
     {
       text: 'Consciously Beautiful',
       linkText: t('SHOP THE LOOK'),
@@ -93,19 +53,23 @@ export default function Slider() {
           text,
           linkText,
           linkHref,
-          imageHref,
           imageSrc,
           buttonClass,
-          paragraphClass = styles.description,
         }, i) => (
           <div key={`slider-${i}`} className={styles.eachSlide}>
             <div className={styles.textcontainer}>
-              <ImageElement imageSrc={imageSrc} imageHref={imageHref} />
+              <Image
+                quality={90}
+                src={imageSrc}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="left top"
+              />
               <h3>{text}</h3>
               <Link href={linkHref}>
                 <a className={buttonClass}>{linkText}</a>
               </Link>
-              <div className={paragraphClass}>
+              <div className={styles.description}>
                 <p>{t('we-design-pieces-to-love')}</p>
               </div>
             </div>
