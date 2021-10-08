c02e0c8e55a3af7c6ca78ca7b4c8f4b3eb47b3e3
Szabolcs Szilágyi <szabolcs.szilagyi@gmx.com>
2021-10-08 23:35:55 +0200

    chore: remove 20% discount

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -253,7 +253,6 @@ export default function Checkout({ productDetailHash }: TCheckoutProps) {
 
     if(newCoupon === 'mynafriend10') newPriceModifier = 0.9;
     else if(newCoupon === 'mynagift15') newPriceModifier = 0.85;
-    else if(newCoupon === 'thespecial20') newPriceModifier = 0.8;
 
     setCoupon(newCoupon)
     setPriceModifier(newPriceModifier);
